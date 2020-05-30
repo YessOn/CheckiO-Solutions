19bea76d03f6fc9233bf7ec9db8f35b9ee8d0578
__Sniper__ <yess.mapss@gmail.com>
2020-05-30 18:47:44 +0100

    Update Barcode Reader.py

--- Barcode Reader.py
+++ Barcode Reader.py
@@ -52,4 +52,6 @@ def barcode_reader(barcode):
     return ''.join(map(str, answer))
 
 print(barcode_reader('_ _   _ __ _  ___ __  __  _  __ ____ _  ___ _ _ _ __  __ __ __  _    _ _ ___  _  ___ _   _  _ _'))
-print(barcode_reader('_ _ ___ __  __  _  _  __ ____ _ _   __ __   _ _ _ _ _    _   _  _  _   ___ _  __  __ __ __  _ _'))
+>> 5901234123457
+print(barcode_reader('_ _   __ _ _  ___ ____ _   _ __  _ ___  ___ _ _ _ __  __ _  ___ _  ___ _ ___  _  _   _ _    _ _'))
+>> 4003994155486
